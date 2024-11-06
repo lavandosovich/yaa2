ad7dc8f0ca8c67a0540909d6a596bac84947076a
mac <mac@MacBook-Pro-mac.local>
2024-11-06 19:31:24 +0300

    sprint-2 add 3 node to shards

--- sharding-repl-cache/scripts/init-router.js
+++ sharding-repl-cache/scripts/init-router.js
@@ -1,6 +1,11 @@
 
 sh.addShard( "shard1/shard1-a-node:27018");
+sh.addShard( "shard1/shard1-b-node:27019");
+sh.addShard( "shard1/shard1-c-node:27023");
 sh.addShard( "shard2/shard2-a-node:27020");
+sh.addShard( "shard2/shard2-b-node:27021");
+sh.addShard( "shard2/shard2-c-node:27024");
+
 
 sh.enableSharding("somedb");
 sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } );
